b4296b07b98d068a67e720a7d2fcb8e28424ad6a
Natalia Szymanska <natsza.ns@gmail.com>
2023-07-10 21:32:08 +0200

    Add scripts.js variables

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
-var computerMove;
-computerMove = "kamień";
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log("wylosowana liczba to: " + randomNumber);
+if (randomNumber == "1") {
+	computerMove = "kamień";
+} else if (randomNumber == "2") {
+	computerMove = "papier";
+} else if (randomNumber == "3") {
+	computerMove = "nożyce";
+}
+printMessage("Mój ruch: " + computerMove);
+
 var playerMove;
 playerMove = "papier";
 printMessage(
 	"Zagrałem " +
 		computerMove +
-		"! Jeśli Twój ruch to" +
+		"! Jeśli Twój ruch to " +
 		playerMove +
 		", to wygrywasz!"
 );
