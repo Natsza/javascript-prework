876254c2ab032b79cd24983331357518f2ef0cfe
Natalia Szymanska <natsza.ns@gmail.com>
2023-07-10 21:48:15 +0200

    Add playerMove and playerInput variable

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+var playerMove, playerInput;
+playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+console.log("Wpisana odpowiedź to: " + playerInput);
+if (playerInput == "1") {
+	playerMove = "kamień";
+} else if (playerInput == "2") {
+	playerMove = "papier";
+} else if (playerInput == "3") {
+	playerMove = "nożyce";
+} else {
+	playerMove = "nieznany ruch";
+}
+printMessage("Twój ruch: " + playerMove);
+
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log("wylosowana liczba to: " + randomNumber);
@@ -10,8 +24,6 @@ if (randomNumber == "1") {
 }
 printMessage("Mój ruch: " + computerMove);
 
-var playerMove;
-playerMove = "papier";
 printMessage(
 	"Zagrałem " +
 		computerMove +
